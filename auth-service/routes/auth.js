3ef72a61c1c36dd0bea9421e67dac9130e5ab50e
alisaid <40726986+si3iid@users.noreply.github.com>
2021-08-19 12:45:47 +1000

    fix nginx load balancer and docker-compose file

--- auth-service/routes/auth.js
+++ auth-service/routes/auth.js
@@ -3,7 +3,7 @@ import { home, login, logout, register, refreshToken, forgotPassword, resetPassw
 import { verifyAccessToken } from '../utils/jwt.js';
 
 const authRoute = express.Router();
-authRoute.get('/', verifyAccessToken, home); //protected with jwt
+authRoute.get('/api/auth/', verifyAccessToken, home); //protected with jwt
 authRoute.post('/api/auth/login', login);
 authRoute.post('/api/auth/logout', logout);
 authRoute.post('/api/auth/register', register);
