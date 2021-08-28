3605d500835cf76d456fe157fe68e9d003bff385
alisaid <40726986+si3iid@users.noreply.github.com>
2021-08-29 02:14:05 +1000

    create profile on register

--- profile/utils/rabbitmq.js
+++ profile/utils/rabbitmq.js
@@ -1,7 +1,8 @@
 import amqp from 'amqplib';
 import mongoose from 'mongoose';
 
-import User from '../models/users.js'
+import User from '../models/users.js';
+import Profile from '../models/profiles.js';
 
 let amqpChannel;
 
@@ -24,17 +25,29 @@ const saveUser = async (message) => ***REMOVED***
     const user = JSON.parse(message.content.toString());
     if (user) ***REMOVED***
         console.log(`New asynchronous message received`);
+        const session = await mongoose.startSession();
+        session.startTransaction();
+        try ***REMOVED***
 
-        const ***REMOVED*** userId, email, username, name ***REMOVED*** = user;
+            const ***REMOVED*** userId, email, username, name ***REMOVED*** = user;
 
-        const doesExist = await User.findOne(***REMOVED*** email ***REMOVED***);
-        if (!doesExist) ***REMOVED***
-            const userData = new User(***REMOVED*** userId, email, username, name ***REMOVED***);
-            const savedUser = await userData.save();
-            if (savedUser && savedUser._id) ***REMOVED***
+            const doesExist = await User.findOne(***REMOVED*** email ***REMOVED***);
+            if (!doesExist) ***REMOVED***
+                const userData = new User(***REMOVED*** userId, email, username, name ***REMOVED***);
+                const profileData = new Profile(***REMOVED*** userId, username, name ***REMOVED***);
+
+                await userData.save(***REMOVED*** session ***REMOVED***);
+                await profileData.save(***REMOVED*** session ***REMOVED***);
+
+                await session.commitTransaction();
+
                 console.log(`persisted event data for _id: $***REMOVED***userId***REMOVED***`);
+
                 amqpChannel.ack(message);
             ***REMOVED***
+        ***REMOVED*** catch (err) ***REMOVED***
+            console.log(`failed to persist user and profile data: $***REMOVED***err***REMOVED***`)
+            await session.abortTransaction();
         ***REMOVED***
     ***REMOVED***
 
